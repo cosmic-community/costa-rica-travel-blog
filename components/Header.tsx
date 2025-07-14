3f072c232b71be8dcb803281a8cc299839073c00
Cosmic AI <support@cosmicjs.com>
2025-07-14 16:17:03 -0700

    Change header navigation link from /posts to /blog

--- components/Header.tsx
+++ components/Header.tsx
@@ -24,7 +24,7 @@ export default function Header() {
             <Link href="/" className="text-gray-700 hover:text-blue-600 transition-colors">
               Home
             </Link>
-            <Link href="/posts" className="text-gray-700 hover:text-blue-600 transition-colors">
+            <Link href="/blog" className="text-gray-700 hover:text-blue-600 transition-colors">
               Blog
             </Link>
             <Link href="/products" className="text-gray-700 hover:text-blue-600 transition-colors">
@@ -64,7 +64,7 @@ export default function Header() {
             <Link href="/" className="text-gray-700 hover:text-blue-600 transition-colors">
               Home
             </Link>
-            <Link href="/posts" className="text-gray-700 hover:text-blue-600 transition-colors">
+            <Link href="/blog" className="text-gray-700 hover:text-blue-600 transition-colors">
               Blog
             </Link>
             <Link href="/products" className="text-gray-700 hover:text-blue-600 transition-colors">
